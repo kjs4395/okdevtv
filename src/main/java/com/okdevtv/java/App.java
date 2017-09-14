b33125e8b4a387e1a892d2ea10c4739a40d6c450
kjs4395 <jisun@jisuns-MacBook-Pro.local>
2017-09-14 21:29:38 +0900

    interface

--- src/main/java/com/okdevtv/java/App.java
+++ src/main/java/com/okdevtv/java/App.java
@@ -4,10 +4,18 @@ package com.okdevtv.java;
  * Hello world!
  *
  */
-public class App 
+public class App implements AppInterface 
 {
+	private static String msg;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       msg = "Hello World";
+       print(msg);
     }
+    private static void print(String s) {
+		System.out.println(s);
+	}
+	public String getString() {
+		return null;
+	}
 }
